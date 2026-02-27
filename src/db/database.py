f163e46ad65b9b594d422aec11a89e0e904f7a4d
Kadyr <Nekketsu4@mail.ru>
2026-02-27 10:16:44 +0300

    feat: added database dependencies

---
+++ src/db/database.py
@@ -0,0 +1,34 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from src.core.config import settings
+
+
+class Base(DeclarativeBase):
+    pass
+
+# Создание асинхронного движка базы данных
+engine = create_async_engine(settings.database_url, echo=False, pool_pre_ping=True)
+
+# Создание фабрики сессий
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False,
+)
+
+
+async def get_async_db_session() -> AsyncGenerator[AsyncSession, None]:
+    """Зависимость для получения сессии базы данных"""
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        
