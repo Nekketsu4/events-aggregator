cd708f17561fa14184fe734120cf8dd942fcb250
Kadyr <Nekketsu4@mail.ru>
2026-03-02 00:40:37 +0300

    feat: added migration

--- src/migration/env.py
+++ src/migration/env.py
@@ -6,9 +6,15 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from src.core.config import settings
+from src.db.database import Base
+
+from src.models import events, sync_metadata
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.get_db)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -19,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
