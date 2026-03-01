8a03289a58c84390d97dc4650dcf3036c6d8fb8f
Kadyr <Nekketsu4@mail.ru>
2026-03-02 00:37:42 +0300

    feat: added config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,3 +1,6 @@
+import os
+
+from loguru import logger
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -6,23 +9,48 @@ class Settings(BaseSettings):
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
-    # Database
-    database_url: str = (
-        "postgresql+asyncpg://postgres:postgres@localhost:5432/events_aggregator"
-    )
+    # Настройки приложения
+    # DEBUG: bool
+    PROJECT_NAME: str
+    VERSION: str
 
-    # # Redis / Celery
-    # redis_url: str = "redis://localhost:6379/0"
+    # REDIS
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DECODE_RESPONSE: bool
 
     # Events Provider API
-    events_provider_base_url: str = "http://events-provider.dev-2.python-labs.ru"
-    events_provider_api_key: str
+    EVENTS_PROVIDER_BASE_URL: str
+    EVENTS_PROVIDER_API_KEY: str
 
-    # # Sync settings
-    # sync_changed_at_default: str = "2000-01-01"
-    #
-    # # Seats cache TTL in seconds
-    # seats_cache_ttl: int = 30
+    # БД
+    POSTGRES_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: int
+    POSTGRES_CONNECTION_STRING: str
+
+    # Логирование
+    FORMAT_LOG: str
+    LOG_ROTATION: str
+
+    @property
+    def get_db(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
 
 settings = Settings()
+
+
+# Настройка логирования
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs", "log.txt")
+logger.add(
+    log_file_path,
+    format=settings.FORMAT_LOG,
+    level="INFO",
+    rotation=settings.LOG_ROTATION,
+)
