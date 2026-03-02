d115cf41239e0fb95af50fb3f04670353409a00c
Kadyr <Nekketsu4@mail.ru>
2026-03-02 15:44:21 +0300

    fixed: Dockerfile config.py

--- src/core/config.py
+++ src/core/config.py
@@ -40,8 +40,8 @@ class Settings(BaseSettings):
     DATABASE_URL: str = ""
 
     # Логирование
-    FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
-    LOG_ROTATION: str = "10 MB"
+    # FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
+    # LOG_ROTATION: str = "10 MB"
 
     @model_validator(mode="after")
     def build_database_url(self):
@@ -83,13 +83,13 @@ class Settings(BaseSettings):
 settings = Settings()
 
 
-# Настройка логирования
-log_file_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "..", "logs", "log.txt"
-)
-logger.add(
-    log_file_path,
-    format=settings.FORMAT_LOG,
-    level="INFO",
-    rotation=settings.LOG_ROTATION,
-)
+# # Настройка логирования
+# log_file_path = os.path.join(
+#     os.path.dirname(os.path.abspath(__file__)), "..", "logs", "log.txt"
+# )
+# logger.add(
+#     log_file_path,
+#     format=settings.FORMAT_LOG,
+#     level="INFO",
+#     rotation=settings.LOG_ROTATION,
+# )
