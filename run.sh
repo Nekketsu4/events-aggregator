fc960425dd66545f99e020438923c5897bc374a3
Kadyr <Nekketsu4@mail.ru>
2026-03-02 19:32:37 +0300

    fixed: run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#set -e
+
+echo "Running database migrations..."
+uv run alembic upgrade head
 
 echo "Starting Uvicorn..."
 exec uv run uvicorn src.main:app --host 0.0.0.0 --port 8000 --workers 1
-
-#wait -n
-#
-#exit $?
