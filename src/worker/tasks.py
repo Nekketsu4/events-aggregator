f08626e7192e5095284ae02a090aff9b568bb67d
Kadyr <Nekketsu4@mail.ru>
2026-03-10 09:37:21 +0300

    refactor: tasks.py
    функция async_sync перенесена в сервис sync_launch и переименована в функцию launch_sync

--- src/worker/tasks.py
+++ src/worker/tasks.py
@@ -1,6 +1,8 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from loguru import logger
 
+from src.service.sync_launch import launch_sync
+
 scheduler = AsyncIOScheduler(timezone="UTC")
 
 
@@ -9,17 +11,6 @@ async def sync_events_task() -> None:
     """Фоновая синхронизация событий API provider в 2 часа ночи"""
     logger.info("Запускаем задачу синхронизации событий")
     try:
-        await async_sync()
+        await launch_sync()
     except Exception as exc:
         logger.exception(f"Запуск задачи синхронизации не удался: {exc}")
-
-
-async def async_sync() -> None:
-    from src.db.database import AsyncSessionLocal
-    from src.service.event_provider_client import EventsProviderClient
-    from src.service.sync_service import SyncService
-
-    client = EventsProviderClient()
-    async with AsyncSessionLocal() as session:
-        service = SyncService(client=client, session=session)
-        await service.run()
