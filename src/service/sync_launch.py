41fdd5ad584dfd49ae0288af159c5de90829d367
Kadyr <Nekketsu4@mail.ru>
2026-03-10 09:35:09 +0300

    feat: added sync_launch.py

---
+++ src/service/sync_launch.py
@@ -0,0 +1,19 @@
+"""
+Вынесена в отдельный модуль чтобы избежать циклических зависимостей
+между src.worker.tasks и src.api
+"""
+
+from src.db.database import AsyncSessionLocal
+from src.service.event_provider_client import EventsProviderClient
+from src.service.sync_service import SyncService
+
+
+async def launch_sync() -> None:
+    """
+    Создаёт сессию БД и запускает полный цикл синхронизации событий.
+    Используется как планировщиком, так и эндпоинтом ручного запуска.
+    """
+    client = EventsProviderClient()
+    async with AsyncSessionLocal() as session:
+        service = SyncService(client=client, session=session)
+        await service.run()
